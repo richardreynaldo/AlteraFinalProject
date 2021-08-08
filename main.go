aa692f72e0ccd9253aa4338e881f361ddec6715b
richardreynaldo <richardreynaldo626@gmail.com>
2021-08-08 15:44:49 +0700

    get all user, register, login

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"finalProject/configs"
-	// "finalProject/routes"
+	"finalProject/routes"
 )
 
 func main() {
 	configs.InitDB()
-	// e := routes.New()
-	// e.Start(":8000")
+	e := routes.New()
+	e.Start(":8000")
 }
