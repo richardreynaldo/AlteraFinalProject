e6262c039f2abfea0c856d9b69464bd519a55fab
richardreynaldo <richardreynaldo626@gmail.com>
2021-08-11 15:59:18 +0700

    fix init db clean architecture

---
+++ app/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+
+	// _newsUsecase "pembayaran/business/news"
+	// _newsController "pembayaran/controllers/news"
+	// _newsRepo "pembayaran/driver/databases/news"
+
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`app/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	// configdb := _dbHelper.ConfigDB{
+	// 	DB_Username: viper.GetString(`database.user`),
+	// 	DB_Password: viper.GetString(`database.pass`),
+	// 	DB_Host:     viper.GetString(`database.host`),
+	// 	DB_Port:     viper.GetString(`database.port`),
+	// 	DB_Database: viper.GetString(`database.name`),
+	// }
+	// db := configdb.InitDB()
+	// timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+
+	e := echo.New()
+
+	// newsRepo := _userRepo.NewMysqlUserRepository(db)
+	// newsUsecase := _userUseCase.NewUserUsecase(newsRepo, timeoutContext)
+	// _userController.NewUserController(e, newsUsecase)
+
+	// eAuth := e.Group("")
+	// eAuth.Use(middleware.JWT([]byte(viper.GetString(`jwt.Key`)))
+	// // jwt
+	// newsRepo := _newsRepo.NewMySQLNewsRepository(db)
+	// newsUsecase := _newsUsecase.NewNewsUsecase(newsRepo, categoryUsecase, ipLocator, timeoutContext)
+	// _newsController.NewNewsController(e, newsUsecase)
+	log.Fatal(e.Start(viper.GetString("server.address")))
+
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module finalProject
 go 1.16
 
 require (
-	github.com/labstack/echo/v4 v4.5.0
-	gorm.io/driver/mysql v1.1.1
-	gorm.io/gorm v1.21.12
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/spf13/viper v1.8.1
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
