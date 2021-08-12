8294882ef5bf952ff45872a08d0b1714dc8a810b
richardreynaldo <richardreynaldo626@gmail.com>
2021-08-13 00:20:16 +0700

    articles mvp

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"finalProject/controllers/articles"
 	"finalProject/controllers/users"
 
 	echo "github.com/labstack/echo/v4"
@@ -8,14 +9,20 @@ import (
 )
 
 type ControllerList struct {
-	JWTMiddleware  middleware.JWTConfig
-	UserController users.UserController
+	JWTMiddleware     middleware.JWTConfig
+	UserController    users.UserController
+	ArticleController articles.ArticleController
 }
 
 func (cl *ControllerList) RouteRegister(e *echo.Echo) {
 	users := e.Group("users")
 	users.POST("/register", cl.UserController.Store)
 	users.GET("/token", cl.UserController.CreateToken)
+
+	article := e.Group("article")
+	article.GET("/list", cl.ArticleController.GetAll)
+	article.POST("/create", cl.ArticleController.Store, middleware.JWTWithConfig(cl.JWTMiddleware))
+	article.PUT("/update", cl.ArticleController.Update, middleware.JWTWithConfig(cl.JWTMiddleware))
 
 	// category := e.Group("category")
 	// category.GET("/list", cl.CategoryController.GetAll, middleware.JWTWithConfig(cl.JWTMiddleware))
--- controllers/users/request/request.go
+++ controllers/users/request/request.go
@@ -10,9 +10,9 @@ type LoginUser struct {
 type RegisterUser struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	Name string `json:"name"`
-	Address string `json:"address"`
-	Role string `json:"role"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	Role     string `json:"role"`
 }
 
 func (req *LoginUser) ToDomain() *users.Domain {
