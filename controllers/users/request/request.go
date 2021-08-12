f2af3ea27f068d1602963e6897e1b9b726d6d56b
richardreynaldo <richardreynaldo626@gmail.com>
2021-08-12 21:38:33 +0700

    login request

--- controllers/users/request/request.go
+++ controllers/users/request/request.go
@@ -7,6 +7,14 @@ type LoginUser struct {
 	Password string `json:"password"`
 }
 
+type RegisterUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name string `json:"name"`
+	Address string `json:"address"`
+	Role string `json:"role"`
+}
+
 func (req *LoginUser) ToDomain() *users.Domain {
 	return &users.Domain{
 		Email:    req.Email,
